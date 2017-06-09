ddf261823425e39c8bd41bdaf92695e3e7fd8cfd
EtiennePlas <etienne.plas@utt.fr>
2017-06-09 15:42:51 +0200

    Constructeurs pour les 3 types de bots, leader suiveur et voyageur

--- models/bot.js
+++ models/bot.js
@@ -1,20 +1,43 @@
-var log = require('log.js');
+//var log = require('log.js');
 module.exports
 {
 
     // Constructeur
-    function Bot () {
-        this.probavoyager = 0.2;
-        this.probatweet
-        this.probaretweet
-        this.nbhashtag
-        this.probalien
-        this.probalike
-        this.probamention
-        this.probaselfmention
-        this.visibilite
-        this.probaphoto
+    function Bot (probavoyager, probatweet, probaretweet, nbhashtag, probalien, probalike, probamention, probaselfmention, visibilite,probaphoto) {
+        this.probavoyager = probavoyager;
+        this.probatweet = probatweet;
+        this.probaretweet =probaretweet;
+        this.nbhashtag = nbhashtag;
+        this.probalien = probalien;
+        this.probalike = probalike;
+        this.probamention = probamention;
+        this.probaselfmention = probaselfmention;
+        this.visibilite = visibilite;
+        this.probaphoto = probaphoto;
     }
+
+    function BotSuiveur() {
+        Bot.call(this, 0.2, 0.2, 0.8, 1, 0.4, 0.8, 0.8, 0.2, 0.05, 0.1 );
+        console.log(this);
+    }
+    BotSuiveur.prototype = Object.create(Bot.prototype);
+    BotSuiveur.prototype.constructor = BotSuiveur;
+
+
+    function BotLeader() {
+        Bot.call(this, 0.2, 0.8, 0.4, 2, 0.6, 0.4, 0.4, 0.8, 0.8, 0.2 );
+        console.log(this);
+    }
+    BotLeader.prototype = Object.create(Bot.prototype);
+    BotLeader.prototype.constructor = BotLeader;
+
+
+    function BotVoyageur() {
+        Bot.call(this, 0.8, 0.6, 0.4, 5, 0.4, 0.4, 0.4, 0.8, 0.15, 0.8 );
+        console.log(this);
+    }
+    BotVoyageur.prototype = Object.create(Bot.prototype);
+    BotVoyageur.prototype.constructor = BotVoyageur;
 
 
     // Ajout de méthode
@@ -36,5 +59,11 @@ module.exports
             }
         };
 
-    Bot.prototype.constructeur = Bot;
-};
+    Bot.prototype.constructor = Bot;
+
+    var bot1 = new BotLeader();
+    var bot2 = new BotSuiveur();
+    var bot3 = new BotVoyageur();
+//bite
+    console.log(bot1,bot2,bot3);
+}
